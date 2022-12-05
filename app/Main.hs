d9d658ae1ad07ca310698a99b7fe64311d970ff0
Luis Aparicio <laparicio9831@gmail.com>
2022-12-05 03:51:58 +0000

    day 4

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import qualified One.One as One
@@ -7,23 +8,55 @@ import System.IO
 import qualified Data.String as String
 import qualified Data.List as List
 import Data.Maybe
+import Data.Ix ( Ix(range) )
 import Data.Function ( on )
+import qualified Data.Bifunctor as Bifunctor
 
 main :: IO ()
 main = do
--- 3-2
-    handle <- openFile "src/three/three_input.txt" ReadMode  
+-- 4-2
+    handle <- openFile "src/four/four_input.txt" ReadMode  
     file <- hGetContents' handle
-    let indexed = String.lines file
-        grouped = filter (/= []) . snd $ 
-            foldr (\curr (old, new) -> (drop 3 old, take 3 old : new)) (indexed, []) indexed 
-        priorities = zip ['a' .. 'z'] [1 .. 26] ++ zip ['A' .. 'Z'] [27 .. 52]
-        commons = map (\case 
-            [a, b, c] -> head $ List.filter (\y -> elem y $ List.filter (`elem` b) a) c
-            _ -> ' ' ) grouped
-        total = sum $ mapMaybe (`lookup` priorities) commons
+    let lines = String.lines file
+        splitLines :: [((Int, Int), (Int, Int))] = map
+            (Bifunctor.bimap 
+            (Bifunctor.bimap read (read . filter (/= '-')) . List.break ('-' ==)) 
+            (Bifunctor.bimap read (read . filter (/= '-')) . List.break ('-' ==) . filter (/= ','))
+                . List.break (',' ==))
+                    lines 
+        overlaps = filter id $ map (\(a , b) -> not . null $ List.intersect (range a) (range b)) splitLines
+        total = length overlaps
     print total
-    hClose handle 
+    hClose handle
+
+-- 4-1
+    -- handle <- openFile "src/four/four_input.txt" ReadMode  
+    -- file <- hGetContents' handle
+    -- let lines = String.lines file
+    --     splitLines :: [((Int, Int), (Int, Int))] = map
+    --         (Bifunctor.bimap 
+    --         (Bifunctor.bimap read (read . filter (/= '-')) . List.break ('-' ==)) 
+    --         (Bifunctor.bimap read (read . filter (/= '-')) . List.break ('-' ==) . filter (/= ','))
+    --             . List.break (',' ==))
+    --                 lines 
+    --     overlaps = filter id $ map (\(a , b) -> List.isSubsequenceOf (range a) (range b) || List.isSubsequenceOf (range b) (range a)) splitLines
+    --     total = length overlaps
+    -- print total
+    -- hClose handle 
+
+-- 3-2
+    -- handle <- openFile "src/three/three_input.txt" ReadMode  
+    -- file <- hGetContents' handle
+    -- let indexed = String.lines file
+    --     grouped = filter (/= []) . snd $ 
+    --         foldr (\curr (old, new) -> (drop 3 old, take 3 old : new)) (indexed, []) indexed 
+    --     priorities = zip ['a' .. 'z'] [1 .. 26] ++ zip ['A' .. 'Z'] [27 .. 52]
+    --     commons = map (\case 
+    --         [a, b, c] -> head $ List.filter (\y -> elem y $ List.filter (`elem` b) a) c
+    --         _ -> ' ' ) grouped
+    --     total = sum $ mapMaybe (`lookup` priorities) commons
+    -- print total
+    -- hClose handle 
     
 -- 3-1
     -- handle <- openFile "src/three/three_input.txt" ReadMode  
